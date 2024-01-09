da9973004328de18a11f67bce20d489f8dec5dda
davidgutsch <david.gutsch@banno.com>
2024-01-09 14:48:49 -0500

    add nix unstable overlay. and other emacs wares

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
 {
   description = "defualt base home manager modules for export.";
   inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/master";
   };
 
-  outputs = {self, nixpkgs, ...}@inputs: {
+  outputs = {self, nixpkgs, nixpkgs-unstable, ...}@inputs: {
     homeManagerModules = import ./home-manager-modules;
+    overlays = {
+      unstable = (final: prev: {
+        unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+      });
+    };
   };
 }
